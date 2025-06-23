49d37f6482414ca3fda17201f52ecff77666236a
12331 <150719155+Geronimo1231@users.noreply.github.com>
2025-06-23 13:22:41 -0600

    Se subieron nuevos archivos

---
+++ frontend_1/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* Estilos globales opcionales */
+body {
+  margin: 0;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background-color: #f9fafb;
+}
+</style>
---
+++ frontend_1/src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+
+import { ToastPlugin } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+
+const app = createApp(App);
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
+
+// Opcional: Puedes pasar opciones al ToastPlugin
+app.use(ToastPlugin, {
+  autoClose: 3000,        // Toasts se cierran en 3 segundos
+  position: 'top-right',  // Ubicación del toast
+})
+
+app.mount('#app')
